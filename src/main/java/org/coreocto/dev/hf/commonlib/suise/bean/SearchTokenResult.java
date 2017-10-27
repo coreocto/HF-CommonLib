1b07b646a93c8c5bc783a7e2515a58c360b08d42
John <coreocto@hotmail.com>
2017-10-27 23:34:56 +0800

    Added missing classes

---
+++ src/main/java/org/coreocto/dev/hf/commonlib/suise/bean/SearchTokenResult.java
@@ -0,0 +1,25 @@
+package org.coreocto.dev.heavensfall.commonlib.suise.bean;
+
+import java.util.List;
+
+public class SearchTokenResult {
+    private String searchToken = null;
+    private List<String> searchHistory = null;
+
+    public String getSearchToken() {
+        return searchToken;
+    }
+
+    public void setSearchToken(String searchToken) {
+        this.searchToken = searchToken;
+    }
+
+    public List<String> getSearchHistory() {
+        return searchHistory;
+    }
+
+    public void setSearchHistory(List<String> searchHistory) {
+        this.searchHistory = searchHistory;
+    }
+
+}
