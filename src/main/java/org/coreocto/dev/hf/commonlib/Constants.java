0aadb61cf76bd55ab1e7c90f2a607748bbcb777c
coreocto <coreocto@hotmail.com>
2017-12-25 22:57:32 +0800

    Added several constants for client/server communication

---
+++ src/main/java/org/coreocto/dev/hf/commonlib/Constants.java
@@ -0,0 +1,18 @@
+package org.coreocto.dev.hf.commonlib;
+
+public class Constants {
+    public static final int SSE_TYPE_SUISE = 0;
+    public static final int SSE_TYPE_VASST = 1;
+
+    public static final int FILE_TYPE_TEXT = 0;
+    public static final int FILE_TYPE_PDF = 1;
+    public static final int FILE_TYPE_DOC = 2;
+
+    public static final String MIME_TYPE_TEXT = "text/plain";
+    public static final String MIME_TYPE_PDF = "application/pdf";
+    public static final String MIME_TYPE_DOC = "application/msword";
+
+    public static final String FILE_EXT_TEXT = "txt";
+    public static final String FILE_EXT_PDF = "pdf";
+    public static final String FILE_EXT_DOC = "doc";
+}
