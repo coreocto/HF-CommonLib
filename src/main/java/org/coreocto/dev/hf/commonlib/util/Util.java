4f47f9bc1104a1e8d208a95a00503e0515f604cd
coreocto <coreocto@hotmail.com>
2018-01-10 16:20:18 +0800

    Added code for copying data from stream to stream

--- src/main/java/org/coreocto/dev/hf/commonlib/util/Util.java
+++ src/main/java/org/coreocto/dev/hf/commonlib/util/Util.java
@@ -1,5 +1,8 @@
 package org.coreocto.dev.hf.commonlib.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.*;
 
 public class Util {
@@ -18,5 +21,21 @@ public class Util {
         }
 
         return substrings;
+    }
+
+    private static final int BUF_SIZE = 0x1000; // 4K
+
+    public static long copy(InputStream from, OutputStream to) throws IOException {
+        byte[] buf = new byte[BUF_SIZE];
+        long total = 0;
+        while (true) {
+            int r = from.read(buf);
+            if (r == -1) {
+                break;
+            }
+            to.write(buf, 0, r);
+            total += r;
+        }
+        return total;
     }
 }
