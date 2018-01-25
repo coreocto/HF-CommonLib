2a53f3915f769f7b5933dda45de8f3e64ad71eb9
coreocto <coreocto@hotmail.com>
2018-01-25 22:21:31 +0800

    Updated IKeyedHashFunc & IHashFunc

--- src/main/java/org/coreocto/dev/hf/commonlib/crypto/IHashFunc.java
+++ src/main/java/org/coreocto/dev/hf/commonlib/crypto/IHashFunc.java
@@ -1,11 +1,10 @@
 package org.coreocto.dev.hf.commonlib.crypto;
 
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public interface IHashFunc {
-    public byte[] getHash(String s) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException;
+    public byte[] getHash(String s) throws UnsupportedEncodingException, NoSuchAlgorithmException;
 
-    public byte[] getHash(byte[] data) throws NoSuchAlgorithmException, InvalidKeyException;
+    public byte[] getHash(byte[] data) throws NoSuchAlgorithmException;
 }
--- src/main/java/org/coreocto/dev/hf/commonlib/crypto/IKeyedHashFunc.java
+++ src/main/java/org/coreocto/dev/hf/commonlib/crypto/IKeyedHashFunc.java
@@ -1,7 +1,11 @@
 package org.coreocto.dev.hf.commonlib.crypto;
 
-public interface IKeyedHashFunc {
-    public byte[] getHash(String key, String s);
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-    public byte[] getHash(byte[] key, byte[] data);
+public interface IKeyedHashFunc {
+    public byte[] getHash(String key, String s) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException;
+
+    public byte[] getHash(byte[] key, byte[] data) throws InvalidKeyException, NoSuchAlgorithmException;
 }
