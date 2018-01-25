7964859ebbc5c630febcfefeb58cb29df12f1d31
coreocto <coreocto@hotmail.com>
2018-01-25 18:30:14 +0800

    Declare possible exception in IHashFunc

--- src/main/java/org/coreocto/dev/hf/commonlib/crypto/IHashFunc.java
+++ src/main/java/org/coreocto/dev/hf/commonlib/crypto/IHashFunc.java
@@ -1,7 +1,11 @@
 package org.coreocto.dev.hf.commonlib.crypto;
 
-public interface IHashFunc {
-    public byte[] getHash(String s);
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-    public byte[] getHash(byte[] data);
+public interface IHashFunc {
+    public byte[] getHash(String s) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException;
+
+    public byte[] getHash(byte[] data) throws NoSuchAlgorithmException, InvalidKeyException;
 }
